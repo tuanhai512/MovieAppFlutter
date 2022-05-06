bcaa23732726a6c88e75a1c45ebf4190c1836475
Hai Tran Tuan <59082486+tuanhai512@users.noreply.github.com>
2022-05-06 09:40:35 +0700

    update youtube

--- movie_app/lib/details/components/body.dart
+++ movie_app/lib/details/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/details/components/cast_and_crew.dart';
 import 'package:movie_app/model/movie.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'backdrop_rating.dart';
 // import 'cast_and_crew.dart';
@@ -9,7 +10,7 @@ import 'title_duration_and_fav_btn.dart';
 class Body extends StatelessWidget {
   final Movie movie;
 
-  const Body({Key? key,required this.movie}) : super(key: key);
+  const Body({Key? key, required this.movie}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // it will provide us total height and width
@@ -21,18 +22,27 @@ class Body extends StatelessWidget {
           BackdropAndRating(size: size, movie: movie),
           SizedBox(height: 20 / 2),
           TitleDurationAndFabBtn(movie: movie),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 20 / 2,
-              horizontal: 20,
-            ),
-            child: Text(
-              "Plot Summary",
-              style: Theme.of(context).textTheme.headline5,
+          Container(
+            child: FlatButton(
+              onPressed: () async {
+                final youtubeUrl =
+                    "https://www.youtube.com/watch?v=761uRaAoW00";
+                if (await canLaunch(youtubeUrl)) {
+                  launch(youtubeUrl);
+                }
+              },
+              color: Color(0xFFFE6D8E),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: Icon(
+                Icons.add,
+                size: 28,
+                color: Colors.white,
+              ),
             ),
           ),
 
-         // CastAndCrew(casts: movie.cast),
+          // CastAndCrew(casts: movie.cast),
         ],
       ),
     );
--- movie_app/test/widget_test.dart
+++ movie_app/test/widget_test.dart
@@ -17,7 +17,7 @@ import 'package:http/http.dart' as http;
 void main() {
   test("Test api", () async {
     Client client = Client();
-    http.Response response = await client.getLatest();
+    http.Response response = await client.getPopular();
     if (response.statusCode == 200) {
       final json = jsonDecode(response.body);
       // final movies = Movie.fromJson(json);
